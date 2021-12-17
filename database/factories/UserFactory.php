f29412f66ce666b9536b19c2c11d3c11e21a2f51
Laratips <aashish201030@gmail.com>
2021-12-17 19:18:23 +0000

    remove doc blocks

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,18 +8,8 @@ use Asdh\PasswordChangedNotification\Tests\Model\User;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
@@ -32,11 +22,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
